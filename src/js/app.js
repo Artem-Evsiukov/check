45384d54c5531868ed5611b154fb7f0ecd9f9166
maluy <artem.evsiukov1@gmail.com>
2022-06-21 17:57:39 +0300

    add_new-files

--- src/js/app.js
+++ src/js/app.js
@@ -13,5 +13,83 @@
   });
 })(jQuery);
 
-import { MDCChipSet } from '@material/chips';
-const chipset = new MDCChipSet(document.querySelector('.mdc-evolution-chip-set'));
+// Hide the element with classToHide
+function hide(classToHide) {
+  [].forEach.call(document.getElementsByClassName(classToHide), function (el) {
+    el.hidden = true;
+  });
+}
+
+// Show the element with classToShow
+function show(classToShow) {
+  [].forEach.call(document.getElementsByClassName(classToShow), function (el) {
+    el.hidden = false;
+  });
+}
+
+// Event for the .com checkbox
+document.getElementById('1').onchange = function () {
+  if (this.checked) show('1');
+  else hide('1');
+};
+
+// Event for the .io checkbox
+document.getElementById('2').onchange = function () {
+  if (this.checked) show('2');
+  else hide('2');
+};
+
+// Event for the .com checkbox
+document.getElementById('3').onchange = function () {
+  if (this.checked) show('3');
+  else hide('3');
+};
+
+// Event for the .io checkbox
+document.getElementById('4').onchange = function () {
+  if (this.checked) show('4');
+  else hide('4');
+};
+
+// Event for the .org checkbox
+document.getElementById('5').onchange = function () {
+  if (this.checked) show('5');
+  else hide('5');
+};
+
+// ::: Events :::
+// Event for the .com checkbox
+document.getElementById('6').onchange = function () {
+  if (this.checked) show('6');
+  else hide('6');
+};
+
+// Event for the .io checkbox
+document.getElementById('7').onchange = function () {
+  if (this.checked) show('7');
+  else hide('7');
+};
+
+// Event for the .org checkbox
+document.getElementById('8').onchange = function () {
+  if (this.checked) show('8');
+  else hide('8');
+};
+
+// Event for the .com checkbox
+document.getElementById('9').onchange = function () {
+  if (this.checked) show('9');
+  else hide('9');
+};
+
+// Event for the .io checkbox
+document.getElementById('10').onchange = function () {
+  if (this.checked) show('10');
+  else hide('10');
+};
+
+// Event for the .org checkbox
+document.getElementById('11').onchange = function () {
+  if (this.checked) show('11');
+  else hide('11');
+};
